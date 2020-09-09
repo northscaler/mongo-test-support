4fc8fe22998f6579465a5c16a121c14c824f8106
Matthew Adams <matthew@matthewadams.me>
2020-09-09 17:05:01 +0000

    Issues/3 drop collections method

--- src/test/integration/mongo/mongo.spec.js
+++ src/test/integration/mongo/mongo.spec.js
@@ -5,6 +5,7 @@ const chai = require('chai')
 chai.use(require('dirty-chai'))
 const expect = chai.expect
 
+const uuid = require('uuid').v4
 const m = require('../../../main')
 
 describe('integration tests of mongo', function () {
@@ -41,6 +42,57 @@ describe('integration tests of mongo', function () {
       })
 
       expect(db).to.be.ok()
+    })
+  })
+
+  describe('dropCollections', function () {
+    it('should work', async function () {
+      if (process.env.CI) {
+        console.log('not running tests because in CI pipeline')
+        return
+      }
+
+      this.timeout(5000)
+
+      const db = await m.mongoConnect({
+        host: 'localhost',
+        port: process.env.MONGO_TEST_SUPPORT_MONGO_PORT || m.mongoConnect.defaultPort
+      })
+
+      expect(db).to.be.ok()
+
+      async function assertCollection ({ name, exists }) {
+        const names = (await db.collections()).map(it => it.collectionName)
+        const index = names.findIndex(it => it === name)
+
+        if (exists) expect(index).to.be.at.least(0)
+        else expect(index).to.equal(-1)
+      }
+
+      const names = [uuid(), uuid()]
+      await db.createCollection(names[0])
+      assertCollection({ name: names[0], exists: true })
+
+      await m.dropCollections({ db, names: names[0] })
+      assertCollection({ name: names[0], exists: false })
+
+      await db.createCollection(names[0])
+      assertCollection({ name: names[0], exists: true })
+      await db.createCollection(names[1])
+      assertCollection({ name: names[1], exists: true })
+
+      await m.dropCollections({ db })
+      assertCollection({ name: names[0], exists: false })
+      assertCollection({ name: names[1], exists: false })
+
+      await db.createCollection(names[0])
+      assertCollection({ name: names[0], exists: true })
+      await db.createCollection(names[1])
+      assertCollection({ name: names[1], exists: true })
+
+      await m.dropCollections({ db, names: [] })
+      assertCollection({ name: names[0], exists: true })
+      assertCollection({ name: names[1], exists: true })
     })
   })
 })
